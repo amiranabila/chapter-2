e75e09bbcac11bdcaa49f0f4d9f8231779434d66
amiranabila <amiranabila45537@gmail.com>
2023-11-30 04:05:08 +0700

    amira nabila

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const res = require("express/lib/response");
 const path = require('path')
 const app = express()
 const port = 5000
+const config = require('./src/config/config.json')
+const {QueryTypes} = require('sequelize');
+const {Sequelize} = require('sequelize');
+const sequelize = new Sequelize(config.development)
+
 
 app.set("view engine", "hbs")
-app.set("views", path.join(__dirname,'views'))
-app.use(express.static('asset')) 
+app.set("views", path.join(__dirname,'src/views'))
+app.use(express.static('src/asset')) 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/',home )
+app.get('/', home )
 app.get('/contact', contact)
 app.post('/add-myproject', addmyproject)
 app.get('/add-myproject', addmyprojectView)
@@ -25,71 +30,85 @@ app.get('/detail-project/:id', detailproject)
 
 
 const data=[]
-function home(req, res) {
-    res.render('index',{data})
+async function home(req, res) {
+  const query = 'SELECT * FROM projects'
+  const obj = await sequelize.query(query, { type: QueryTypes.SELECT })
+  console.log ("ini database", obj)
+    res.render('index',{data:obj})
 }
 function contact (req, res) {
     res.render('contact-me')
 }
 function addmyprojectView (req, res) {
-    res.render('add-myproject')
+  
+    res.render('add-myproject', {data})
 }
-function addmyproject(req, res) {
+async function addmyproject(req, res) {
+
     const { name, startDate, endDate, description } = req.body;
-  
-    console.log("Nama Project :", name);
-    console.log("Tanggal Mulai : ", startDate);
-    console.log("Tanggal Selesai : ", endDate);
-    console.log("Deskripsi : ", description);
-  
-    const dataProject = { name, startDate, endDate, description };
-  
-    data.unshift(dataProject);
-  
-    res.redirect("/");
-  }
-  
-  function deleteProject(req, res) {
-    const { id } = req.params;
-    data.splice(id, 1);
-  
-    res.redirect("/");
-  }
-  
-  function updateProjectview(req, res) {
-    const { id } = req.params
-    const dataFilter = data[parseInt(id)]
-    dataFilter.id = parseInt(id)
-    res.render("update-project", { data: dataFilter })
-  }
-  
-  function updateProject(req, res) {
-    const { id, name, startDate, endDate, description } = req.body;
+    const image = "dumbways.png"
   
     // console.log("Nama Project :", name);
     // console.log("Tanggal Mulai : ", startDate);
     // console.log("Tanggal Selesai : ", endDate);
     // console.log("Deskripsi : ", description);
   
-    // const dataProject = { id, name, startDate, endDate, description };
+    // const dataProject = { name, startDate, endDate, description };
   
     // data.unshift(dataProject);
+    const query = `INSERT INTO projects (name, start_date, end_date, image, description) VALUES ('${name}', '${startDate}', '${endDate}', '${image}', '${description}')`
+    const obj = await sequelize.query(query, { type: QueryTypes.INSERT })
+
+    console.log("data berhasil di insert", obj)
+    res.redirect("/");
+  }
   
-    data[parseInt(id)] = {
-      name,
-      startDate,
-      endDate,
-      description,
-    };
+ async function deleteProject(req, res) {
+    const { id } = req.params;
+    const query = `DELETE FROM projects WHERE id=${id} `
+    const obj = await sequelize.query(query, { type: QueryTypes.DELETE })
+    console.log("data berhasil di hapus", obj)
+
+    // data.splice(id, 1);
   
     res.redirect("/");
   }
   
-  function detailproject(req, res) {
+  async function updateProjectview(req, res) {
+    const { id } = req.params
+    // const dataFilter = data[parseInt(id)]
+    // dataFilter.id = parseInt(id)
+    const query = `SELECT * FROM projects WHERE id=${id}`
+    const obj = await sequelize.query(query, {type:QueryTypes.SELECT})
+    
+    res.render("update-project", { data: obj[0] })
+  }
+  
+  async function updateProject(req, res) {
+    const { id, name, startDate, endDate, description } = req.body;
+  
+    
+    // data[parseInt(id)] = {
+    //   name,
+    //   startDate,
+    //   endDate,
+    //   description,
+    // };
+    const query = `UPDATE projects SET name='${name}', start_date='${startDate}', end_date='${endDate}', description='${description}' WHERE id=${id}`
+    const obj = await sequelize.query(query, {type:QueryTypes.UPDATE})
+    console.log("blog berhasil diupdate", obj)
+  
+    res.redirect("/");
+  }
+  
+ async function detailproject(req, res) {
     const { id } = req.params;
-    const dataFilter = data[parseInt(id)];
-    dataFilter.id = parseInt(id);
-    res.render("detail-project", { data: dataFilter });
+    const query = `SELECT * FROM projects WHERE id=${id}`
+    const obj = await sequelize.query(query, {type:QueryTypes.SELECT})
+    res.render('detail-project', { data: obj[0] })
+    // const dataFilter = data[parseInt(id)];
+    // dataFilter.id = parseInt(id);
+    // res.render("detail-project", { data: dataFilter });
   }
 // function detail (req, res) {
 //     res.render('detail')
@@ -134,3 +153,7 @@ function addmyproject(req, res) {
 app.listen(port, () =>{
     console.log(`server berjalan diport ${port}`)
 })
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  // Lakukan penanganan kesalahan sesuai kebutuhan Anda
+});

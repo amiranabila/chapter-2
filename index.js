ac2cf68e453d1c6118969ccb2ceb290966169431
amiranabila <amiranabila45537@gmail.com>
2023-11-30 10:08:27 +0700

    amira nabila

--- index.js
+++ index.js
@@ -9,14 +9,30 @@ const config = require('./src/config/config.json')
 const {QueryTypes} = require('sequelize');
 const {Sequelize} = require('sequelize');
 const sequelize = new Sequelize(config.development)
-
+const bcrypt = require ('bcrypt')
+const session = require('express-session')
+const flash = require('express-flash')
 
 app.set("view engine", "hbs")
 app.set("views", path.join(__dirname,'src/views'))
 app.use(express.static('src/asset')) 
 app.use(express.urlencoded({ extended: false }));
-
+app.use(flash())
+app.use(session ({
+  name:"data",
+  secret: 'rahasia',
+  resave: false,
+  saveUninitialized: true,
+  cookie:{
+    secure: false,
+    maxAge : 1000 * 60 * 60 * 24
+  }
+}))
 app.get('/', home )
+app.get('/register', registerView)
+app.post('/register', register)
+app.get('/login', loginView)
+app.post('/login', login)
 app.get('/contact', contact)
 app.post('/add-myproject', addmyproject)
 app.get('/add-myproject', addmyprojectView)
@@ -34,14 +50,76 @@ async function home(req, res) {
   const query = 'SELECT * FROM projects'
   const obj = await sequelize.query(query, { type: QueryTypes.SELECT })
   console.log ("ini database", obj)
-    res.render('index',{data:obj})
+  const isLogin = req.session.isLogin
+  const user = req.session.user
+    res.render('index',{data:obj, user: req.session.user, isLogin, user})
+}
+function registerView (req, res) {
+  res.render('register')
+}
+async function register (req, res) {
+  const { name, email, password} =req.body
+
+  console.log("nama", name)
+  console.log("email", email)
+  console.log("password", password)
+
+  const salt = 10 
+
+  bcrypt.hash(password, salt, async (err, hash) =>{
+    if (err){
+       req.flash('danger', 'register failed!')
+      console.error("pasword tidak bisa dienkripsi")
+      return res.redirect('/register')
+
+    }
+    console.log("hasil result:", hash)
+    const query = `INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${hash}')`
+    await sequelize.query(query, { type: QueryTypes.INSERT })
+    req.flash('success', 'register success!')
+    res.redirect('/login')
+    
+  })
+  
+}
+function loginView (req, res) {
+  res.render('login')
+}
+async function login (req, res) {
+ const {email, password} = req.body
+ const query = `SELECT * FROM users WHERE email='${email}'`
+ const obj = await sequelize.query(query, { type: QueryTypes. SELECT})
+
+  if(!obj.length){
+    console.error("user not registed")
+    return res.redirect('/login')
+  }
+  bcrypt.compare(password, obj[0].password, (err, result) => {
+   
+    if (!result){
+      console.error("pasword salah")
+      req.flash('danger', 'login failed!')
+      return res.redirect('/login')
+    }
+    console.log('login berhasil')
+    req.session.isLogin = true
+    req.flash('success', 'login success!')
+    req.session.user = {
+      name: obj[0].name,
+      email: obj[0].email
+    }
+    res.redirect('/')
+  })
 }
 function contact (req, res) {
-    res.render('contact-me')
+    const isLogin = req.session.isLogin
+    const user = req.session.user
+    res.render('contact-me', {data, isLogin, user})
 }
 function addmyprojectView (req, res) {
-  
-    res.render('add-myproject', {data})
+    const isLogin = req.session.isLogin
+    const user = req.session.user
+    res.render('add-myproject', {data, isLogin, user})
 }
 async function addmyproject(req, res) {
 
@@ -80,8 +158,10 @@ async function addmyproject(req, res) {
     // dataFilter.id = parseInt(id)
     const query = `SELECT * FROM projects WHERE id=${id}`
     const obj = await sequelize.query(query, {type:QueryTypes.SELECT})
+    const isLogin = req.session.isLogin
+    const user = req.session.user
     
-    res.render("update-project", { data: obj[0] })
+    res.render("update-project", { data: obj[0], isLogin, user })
   }
   
   async function updateProject(req, res) {
@@ -105,7 +185,9 @@ async function addmyproject(req, res) {
     const { id } = req.params;
     const query = `SELECT * FROM projects WHERE id=${id}`
     const obj = await sequelize.query(query, {type:QueryTypes.SELECT})
-    res.render('detail-project', { data: obj[0] })
+    const isLogin = req.session.isLogin
+    const user = req.session.user
+    res.render('detail-project', { data: obj[0], isLogin, user })
     // const dataFilter = data[parseInt(id)];
     // dataFilter.id = parseInt(id);
     // res.render("detail-project", { data: dataFilter });
